64b6458e69ca09646142ef0295fe9bfb7519e1a7
Nathaniel McCallum <npmccallum@redhat.com>
2017-08-31 19:56:04 -0400

    Initial Commit

---
+++ FreeU2F/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FreeU2F
+//
+//  Created by Nathaniel McCallum on 8/31/17.
+//  Copyright © 2017 Nathaniel McCallum. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FreeU2F/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FreeU2F
+//
+//  Created by Nathaniel McCallum on 8/31/17.
+//  Copyright © 2017 Nathaniel McCallum. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
